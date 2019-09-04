4e9701cc7d94a0af36402ff3fc93129b73a96a75
prabath <prabath@wso2.com>
2019-09-04 11:53:02 -0700

    updates

--- ch05/sample02/src/main/java/com/apress/ch05/sample02/GatewayApplication.java
+++ ch05/sample02/src/main/java/com/apress/ch05/sample02/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com.apress.ch05.sample01;
+package com.apress.ch05.sample02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
