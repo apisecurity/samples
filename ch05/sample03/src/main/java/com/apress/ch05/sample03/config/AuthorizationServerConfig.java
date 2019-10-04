426d944b66d601b7920d4a826138cfb6e03e7b03
prabath <prabath@wso2.com>
2019-10-04 11:59:53 -0700

    updates

--- ch05/sample03/src/main/java/com/apress/ch05/sample03/config/AuthorizationServerConfig.java
+++ ch05/sample03/src/main/java/com/apress/ch05/sample03/config/AuthorizationServerConfig.java
@@ -1,10 +1,12 @@
 package com.apress.ch05.sample03.config;
 
+import java.io.File;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -44,7 +46,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory().withClient("10101010").secret("11110000").scopes("foo", "bar")
-				.authorizedGrantTypes("client_credentials", "password", "refresh_token").accessTokenValiditySeconds(6000);
+				.authorizedGrantTypes("client_credentials", "password", "refresh_token")
+				.accessTokenValiditySeconds(6000);
 	}
 
 	@Bean
@@ -62,9 +65,10 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		String pwd = environment.getProperty("spring.security.oauth.jwt.keystore.password");
 		String alias = environment.getProperty("spring.security.oauth.jwt.keystore.alias");
 		String keystore = environment.getProperty("spring.security.oauth.jwt.keystore.name");
+		String path = System.getProperty("user.dir");
 
-		KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource(keystore),
-				pwd.toCharArray());
+		KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(
+				new FileSystemResource(new File(path + File.separator + keystore)), pwd.toCharArray());
 		JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
 		converter.setKeyPair(keyStoreKeyFactory.getKeyPair(alias));
 		return converter;
